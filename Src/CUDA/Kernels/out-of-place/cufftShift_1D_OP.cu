a0c2070ec7d841f0a77b0a656ef30fbb6fc89886
marwan-abdellah <abdellah.marwan@gmail.com>
2014-01-15 09:20:40 +0100

    Adding the Licence to the project.

--- Src/CUDA/Kernels/out-of-place/cufftShift_1D_OP.cu
+++ Src/CUDA/Kernels/out-of-place/cufftShift_1D_OP.cu
@@ -1,3 +1,22 @@
+/*********************************************************************
+ * Copyright © 2011-2014,
+ * Marwan Abdellah: <abdellah.marwan@gmail.com>
+ *
+ * This library (cufftShift) is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ ********************************************************************/
+ 
 #ifndef CUFFTSHIFT_1D_CU
 #define CUFFTSHIFT_1D_CU
 
